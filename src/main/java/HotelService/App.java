a2ea7192f164e6a1bb986aa42d4eeea74fdf3b65
Kamil Marcinow <kamil.marcinow@gmail.com>
2019-02-23 12:59:13 +0100

    first

---
+++ src/main/java/HotelService/App.java
@@ -0,0 +1,13 @@
+package HotelService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
